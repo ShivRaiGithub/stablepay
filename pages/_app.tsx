1a6b83631236b68a3ba947d0ff85971b559d17c5
Shiv Rai <sshaktirai@gmail.com>
2025-02-18 11:48:08 +0530

    Cleaned

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { PrivyProvider } from "@privy-io/react-auth";
-
-import {defineChain} from 'viem';
-
-import {mainnet} from 'viem/chains';
-
-import {addRpcUrlOverrideToChain} from '@privy-io/react-auth';
-
-const mainnetOverride = addRpcUrlOverrideToChain(mainnet, "http://127.0.0.1:8545");
-
-export const myCustomChain = defineChain({
-  id: 31337, // Replace this with your chain's ID
-  name: 'Foundry Local',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'FoundryETH',
-    symbol: 'FETH',
-  },
-  rpcUrls: {
-    default: {
-      http: ['http://127.0.0.1:8545'],
-    },
-  },
-  blockExplorers: {
-    default: {name: 'LocalExplorer', url: 'etherscan.io'},
-  },
-});
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -67,19 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Privy Auth Starter</title>
         <meta name="description" content="Privy Auth Starter" />
       </Head>
-      
-      <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
-        config={{
-          defaultChain: myCustomChain, 
-          embeddedWallets: {
-            createOnLogin: "all-users",
-          },
-          supportedChains: [mainnetOverride, myCustomChain],
-        }}
-      >
         <Component {...pageProps} />
-      </PrivyProvider>
     </>
   );
 }
